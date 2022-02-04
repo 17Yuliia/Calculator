c7b0e3e2ed8cc4d753f6496a4752d03fdd4275c3
17Yuliia <ahalakova1701@gmail.com>
2022-02-04 19:57:14 +0200

    added validation for input

--- calculator.js
+++ calculator.js
@@ -1,10 +1,56 @@
 class Calculator {
     constructor(){
-        this.input = '';
+        this.input = '0';
     };
 
-    AddSymbol(symbol) {
-        this.input = this.input + symbol;
+    Append(symbol) {
+        this.input += symbol;
+    }
+
+    ReplaceLast(symbol) {
+        this.input = this.input.slice(0,-1) + symbol;
+    }
+
+    getInputNumbers(symbol) {
+        const spliters = /[+\-%×÷]/g
+        const numbersArray = (this.input + symbol).split(spliters);
+        
+        return numbersArray;
+    }
+
+    ValidateInput(symbol) {
+        const isNaNSymbol = isNaN(symbol);
+
+        if(this.input == '0') {
+            isNaNSymbol ? this.Append(symbol) : this.ReplaceLast(symbol);
+        }
+        else {
+            const lastSymbol = this.input.slice(-1);
+            const isNaNLastSymbol = isNaN(lastSymbol);
+            const inputNumbers = this.getInputNumbers(symbol);
+
+            if(symbol == ',' && isNaNLastSymbol){
+                this.Append('0'+symbol);
+                return;
+            }
+
+            const lastNumber = inputNumbers.slice(-1)[0].slice(0, -1);
+
+            if((symbol == ',' && lastNumber.includes(',')) ||
+                (symbol == '0' && lastNumber == '0')
+            ){ 
+                return;
+            }
+
+            if((lastNumber == '0' && !isNaNSymbol) ||
+                (isNaNSymbol && isNaNLastSymbol)
+            ){
+                this.ReplaceLast(symbol);
+                return;
+            }
+
+            this.Append(symbol);                
+        }
     }
 
     GetInput() {
@@ -12,23 +58,7 @@ class Calculator {
     }
 
     ClearInput() {
-        this.input = '';
-    }
-    
-    Plus(x, y) {
-        return x + y;
-    }
-
-    Minus(x, y) {
-        return x - y;
-    }
-
-    Multiply(x, y) {
-        return x * y;
-    }
-
-    Divide(x, y) {
-        return x / y;
+        this.input = '0';
     }
 
     CalcResult() {
--- calculator_logic.js
+++ calculator_logic.js
@@ -1,13 +1,15 @@
 document.body.classList.remove('preload');
 const display = document.getElementById('display');
+const displayText = Array.from(document.getElementsByClassName('display-block__text'))[0];
+console.log(displayText);
 
 let calculator = new Calculator();
 
 function addSymbol(value) {
-    console.log('value', value);
-    calculator.AddSymbol(value);
+    calculator.ValidateInput(value);
     const input = calculator.GetInput();
     ShowOnDisplay(input);
+    displayText.scrollTop = displayText.scrollHeight;
 }
 
 function ShowOnDisplay(text) {
@@ -15,8 +17,12 @@ function ShowOnDisplay(text) {
 }
 
 function clearDisplay() {
-    display.textContent = '';
+    display.textContent = '0';
     calculator.ClearInput();
+}
+
+function calculateResult() {
+    calculator.calculateResult();
 }
 
 function init() {
@@ -27,6 +33,7 @@ function init() {
     cancelButton.onclick = clearDisplay;
 
     const resultButton = buttonsArray.pop();
+    resultButton.onclick = calculateResult;
 
     const printedButtons = buttonsArray;
     printedButtons.forEach(btn => {
